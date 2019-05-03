03168bc9938b2f62da7578b415f39507f0eb6ef2
Gema Ulama Putra <cybermujahidz@gmail.com>
2019-05-03 15:10:06 +0700

    kategori:fix card header title

--- frontend/pages/master-kategori.php
+++ frontend/pages/master-kategori.php
@@ -9,8 +9,8 @@
 
     <!-- DataTales Example -->
     <div class="card shadow mb-4 datatable-card">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+        <div class="card-header py-3">  
+            <h6 class="m-0 font-weight-bold text-primary">Data Kategori</h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
